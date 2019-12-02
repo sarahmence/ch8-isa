4ef2987b3cb9a51e1b8efb4a700c16459db98c64
ahdavis <ahdavis13@gmail.com>
2019-12-02 11:41:56 -0600

    Added CLSData struct

---
+++ src/data/cls_data.rs
@@ -0,0 +1,55 @@
+/*
+ * cls_data.rs
+ * Defines a struct that holds data for the CLS instruction
+ * Created on 12/2/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//usage statement
+use super::super::codegen::CodeGen;
+
+/// Contextual data for the `CLS` instruction
+pub struct CLSData {
+    //no fields
+}
+
+//implementation
+impl CLSData {
+    /// Creates a new `CLSData` instance
+    ///
+    /// # Returns
+    ///
+    /// A new `CLSData` instance
+    pub fn new() -> CLSData {
+        return CLSData { };
+    }
+}
+
+//CodeGen implementation
+impl CodeGen for CLSData {
+    /// Generates the opcode for the instruction
+    /// 
+    /// # Returns 
+    ///
+    /// The opcode for the `CLS` instruction
+    fn gen_opcode(&self) -> u16 {
+        return 0x00E0;
+    }
+}
+
+//end of file
