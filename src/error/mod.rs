28a4b141f2f73b515d4e63885d4c2229847a3ca1
ahdavis <ahdavis13@gmail.com>
2019-12-06 06:12:36 -0600

    Added error module header

---
+++ src/error/mod.rs
@@ -0,0 +1,29 @@
+/*
+ * mod.rs
+ * Module export file for the codegen module
+ * Created on 12/2/2019
+ * Created by Andrew Davis
+ *
+ * Copyright (C) 2019  Andrew Davis
+ *
+ * This program is free software: you can redistribute it and/or modify   
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//exports
+mod binary_error;
+pub use binary_error::BinaryError;
+mod binary_error_type;
+pub use binary_error_type::BinaryErrorType;
+
+//end of file
